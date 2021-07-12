feffc3bb2ee8c183bdb89d7c8f2df623ddeae75a
matkatmusic <matkatmusic@gmail.com>
2021-07-12 18:58:28 +0000

    Found cause of output not matching

--- main.cpp
+++ main.cpp
@@ -407,7 +407,7 @@ int main()
     std::cout << "DoubleType add result=" << *dt.add(2.0).value << std::endl;
     std::cout << "DoubleType subtract result=" << *dt.subtract(2.0).value << std::endl;
     std::cout << "DoubleType multiply result=" << *dt.multiply(2.0).value << std::endl;
-    std::cout << "DoubleType divide result=" << *dt.divide(5.).value << std::endl << std::endl;
+    std::cout << "DoubleType divide result=" << *dt.divide(5.f).value << std::endl << std::endl;
 
     std::cout << "IntType add result=" << *it.add(2).value << std::endl;
     std::cout << "IntType subtract result=" << *it.subtract(2).value << std::endl;
